346ddd3f7e285fb418c766e46129fc8cd2babd8e
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-04-04 14:00:06 +0700

    Update + Restore HTML, import CSS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log("Hello World!");
+import './css/styles.css';
+import Groceries from './js/groceries.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+  function init() {
+    const groceriesList = document.getElementById('groceriesList');
+
+    const groceries = Groceries.getItems();
+
+    groceries.forEach((grocery) => {
+      const el = Groceries.createItem(grocery);
+      groceriesList.appendChild(el);
+    });
+  }
+
+  const groceriesForm = document.getElementById('groceriesForm');
+  const groceriesName = document.getElementById('groceriesName');
+  const groceriesQuantity = document.getElementById('groceriesQuantity');
+
+  groceriesForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    Groceries.createItem({
+      name: groceriesName.value,
+      quantity: groceriesQuantity.value,
+    });
+
+    groceriesName.value = '';
+    groceriesQuantity.value = '';
+  });
+
+  init();
+});
